3d6ac94570c76dadc78f9379d821e8821f5fcc17
nwics <nabilwicaksono274@gmail.com>
2026-03-16 04:14:16 +0700

    fixing vite

--- lebaranv2-app/src/App.jsx
+++ lebaranv2-app/src/App.jsx
@@ -21,7 +21,7 @@ const FontLink = () => {
   return null;
 };
 
-const TARGET_DATE = new Date(2026, 1, 21, 6, 0, 0);
+const TARGET_DATE = new Date(2026, 2, 21, 6, 0, 0);
 
 const getTimeLeft = (target) => {
   const diff = target - Date.now();
